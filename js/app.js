67a72ffd97d9bf9337d264e5612baca269686464
Cassandra <Cassy@Stephens-MacBook-Pro-2.local>
2020-02-03 16:55:44 -0800

    updated Totals column and grand totals

--- js/app.js
+++ js/app.js
@@ -1,30 +1,31 @@
 'use strict';
+// LAB 07 
+// ==============================
+// 1. Replace all of your object literals for the salmon cookie stand with a single constructor function that, when called with the 'new' keyword, it creates a new instance.
 
-// 1. Stores the min/max hourly customers, and the average cookies per customer, in object properties
-// 2. Uses a method of that object to generate a random number of customers per hour. [Objects/Math/random](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random){:target="_blank"} 
-// 3. Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
-// 4. Store the results for each location in a separate array... perhaps as a property of the object representing that location
-// 5. Display the values of each array as unordered lists in the browser
-// 6. Calculating the sum of these hourly totals; your output for each location should look like this:
+// 2. Replace the lists of your data for each store and build a single table of data instead. It shoudl look similar to the following:
 
-//     Seattle
-//     - 6am: 16 cookies
-//     - 7am: 20 cookies
-//     - 8am: 35 cookies
-//     - 9am: 48 cookies
-//     - 10am: 56 cookies
-//     - 11am: 77 cookies
-//     - 12pm: 93 cookies
-//     - 1pm: 144 cookies
-//     - 2pm: 119 cookies
-//     - 3pm: 84 cookies
-//     - 4pm: 61 cookies
-//     - 5pm: 23 cookies
-//     - 6pm: 42 cookies
-//     - 7pm: 57 cookies
-//     - Total: 875 cookies
+//  Display each stores data in a table format similar to what is below. Break each column by the hour and complete each row with a "Daily Location Total".
 
-// Here are the starting numbers that you'll need to build these objects:
+
+//          | 6:00 am  | 7:00 am | 8:00 am | 9:00 am | 10:00 am | 11:00 am | 12:00 pm | Daily Location Total | 
+// ---------|----------|---------|---------|---------|----------|----------|----------|----------------------|
+// Seattle  |          |         |         |         |          |          |          |                      |
+// ---------|----------|---------|---------|---------|----------|----------|----------|----------------------|
+// Tokyo    |          |         |         |         |          |          |          |                      |
+// ---------|----------|---------|---------|---------|----------|----------|----------|----------------------|
+// Dubai    |          |         |         |         |          |          |          |                      |
+// ---------|----------|---------|---------|---------|----------|----------|----------|----------------------|
+// Paris    |          |         |         |         |          |          |          |                      |
+// ---------|----------|---------|---------|---------|----------|----------|----------|----------------------|
+// Lima     |          |         |         |         |          |          |          |                      |
+// ---------|----------|---------|---------|---------|----------|----------|----------|----------------------|
+// Totals   |          |         |         |         |          |          |          |                      |
+// ---------|----------|---------|---------|---------|----------|----------|----------|----------------------|
+
+// - Each cookie stand location should have a separate render() method that creates and appends its row to the table
+
+// - The header row and footer row are each created in their own stand-alone function
 
 // Location | Min / Cust | Max / Cust | Avg Cookie / Sale
 // ---------|------------|------------|-------------------
@@ -34,10 +35,12 @@
 // Paris    |      20    |     38     |        2.3
 // Lima     |      2     |     16     |        4.6
 
+
 var operatingHours = ['6am', '7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
 var franchiseLocations = ['Seattle','Tokyo','Dubai','Paris','Lima'];
 var allLocations = [];
 var locationTotals = [];
+var grandTotal = 0;
 var tblBody = document.getElementById('salesByLocation');
 
 // =========================================================
@@ -54,10 +57,14 @@ function tableHeaderSetup(){
         tblHeader.appendChild(headerEl);
         locationTotals[i] = 0;
     };
+    headerEl = document.createElement('th');
+    headerEl.textContent = 'Total Daily Sales';
+    tblHeader.appendChild(headerEl);
 };
 
-// ============================================
-
+// =========================================================
+// Object Constructor (Location)
+// =========================================================
 function Location (name, minCustomers, maxCustomers, avgCookies) {
     this.name = name;
     this.minCustomers = minCustomers;
@@ -67,12 +74,18 @@ function Location (name, minCustomers, maxCustomers, avgCookies) {
     this.hourlyOutput = [];
     allLocations.push(this);
 }
+//----------------------------
+// Prototype Functions
+//----------------------------
 // generates the random number of customers per hour
+//-------------------------------------------------------
 Location.prototype.customerPerHr = function(){
     return (Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers);
-
 };
+
+//----------------------------
 // calculates the amount of cookies per hour / saves to array
+//-----------------------------------------------------------------
 Location.prototype.calcHourlyCookies = function(){
     for (var i = 0; i < operatingHours.length; i++){
         var cookiesPerHour = Math.round(this.customerPerHr() * this.avgCookies);
@@ -80,26 +93,33 @@ Location.prototype.calcHourlyCookies = function(){
         this.totalDailyCookies += cookiesPerHour;
     }
 };
-// adds the calculated sales/hr to the table
-Location.prototype.addSaleToTable = function(){
-    var tblRow = document.createElement('tr'); // this is my location Row
-    tblBody.appendChild(tblRow);
-    var rowHeader = document.createElement('th'); // this is my location name (header/bold)
-    rowHeader.textContent = this.name;
-    tblRow.appendChild(rowHeader);
-    var col = 1; //starting column location [0] is the location name
 
-    for(var i = 0; i < this.hourlyOutput.length; i++){
-        var tblData = document.createElement('td');
-        tblData.textContent = this.hourlyOutput[i];
-        var cell = tblRow.insertCell(col);
-        cell.textContent = this.hourlyOutput[i];
-        locationTotals[i] = locationTotals[i] + this.hourlyOutput[i];
-        col++;
+//----------------------------
+// adds the calculated sales/hr to the table
+//-----------------------------------------------------------------
+Location.prototype.addSaleToTable = function(){
+    var tblRow = document.createElement('tr');      // create my table Row
+    tblBody.appendChild(tblRow);                    // add the table to my global table body
+    var rowHeader = document.createElement('th');   // create a row header
+    rowHeader.textContent = this.name;              // add the location name to the row header
+    tblRow.appendChild(rowHeader);                  // add the row header to my row
+    var col = 1;                                    //starting column location [0] is the location name
+
+    for(var i = 0; i < this.hourlyOutput.length; i++){  // loop through the hourly Cookie sales
+        var cell = tblRow.insertCell(col);                  // create a cell - insert that cell in specific index location
+        cell.textContent = this.hourlyOutput[i];            // give that cell hourly sales
+        locationTotals[i] += this.hourlyOutput[i];          // running total of cookie sales/hour
+        col++;                                              // increasing column index
     }
+    cell = tblRow.insertCell(col);                  // create a cell - insert that cell in specific index location
+    cell.textContent = this.totalDailyCookies;      // give that cell total sales per location
+    cell.id = "locationTotals";
+    grandTotal += this.totalDailyCookies;           // increasing grand total
 };
 
-
+// ============================================
+// - Add in the new objects
+// ============================================
 new Location('Seattle', 23, 65, 6.3);
 new Location('Tokyo', 3, 24, 1.2);
 new Location('Dubai', 11, 38, 3.7);
@@ -107,27 +127,35 @@ new Location('Paris', 20, 38, 2.3);
 new Location('Lima', 2, 16, 4.6);
 
 // ============================================
-// This is the completes my total Row on Sales Table
+// Footer/Total Row Function
 // ============================================
 function tableFooterSetup(){
+
     var tblFooter = document.getElementById('salesTableTotal');
     var tblRow = document.createElement('tr');
     tblFooter.appendChild(tblRow);
-    var rowHeader = document.createElement('th'); // this is my location name (header/bold)
+
+    var rowHeader = document.createElement('th'); 
     rowHeader.textContent = 'Totals:';
     tblRow.appendChild(rowHeader);
     var col = 1;
     
     for(var i = 0; i < locationTotals.length; i++){
-        var tblData = document.createElement('td');
-        tblData.textContent = locationTotals[i];
+        // var tblData = document.createElement('td');
+        // tblData.textContent = locationTotals[i];
         var cell = tblRow.insertCell(col);
         cell.textContent = locationTotals[i];
         col++;
     };
-}
-// ============================================
 
+    cell = tblRow.insertCell(col);
+    cell.id = "grandTotal";
+    cell.textContent = grandTotal;
+}
+
+// ============================================
+// Invoke the functions
+// ============================================
 tableHeaderSetup();
 
 for(var x = 0; x < allLocations.length; x++){
@@ -135,4 +163,5 @@ for(var x = 0; x < allLocations.length; x++){
     allLocations[x].calcHourlyCookies();
     allLocations[x].addSaleToTable();
 }
+
 tableFooterSetup();
